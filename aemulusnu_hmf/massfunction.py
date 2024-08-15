6830102067347c14e3964cd341f9a515ded11ea0
Delon Liu Shen <delonshen@gmail.com>
2024-08-14 17:21:58 -0700

    refined precision settings for CLASS and physical constants

--- aemulusnu_hmf/massfunction.py
+++ aemulusnu_hmf/massfunction.py
@@ -6,7 +6,9 @@ from .utils import scaleToRedshift, redshiftToScale
 import numpy as np
 
 
-G_over_c2 = 4.79e-20 #Mpc / Msol
+_G_ = 6.67428e-11  # Newton constant in m^3/Kg/s^2 (CLASS precision)
+_c_ = 2.99792458e8 # speed of light in m/s         (CLASS precision)
+G_over_c2 = 4.78538e-20 #Mpc / Msol
 
 class cosmology:
     def __init__(self, cosmology):
@@ -26,6 +28,7 @@ class cosmology:
             'P_k_max_h/Mpc': 20.,
             'm_ncdm': self.cosmology['nu_mass_ev']/3,
             'deg_ncdm': 3,
+            'ncdm_quadrature_strategy': 2,
             'T_cmb': 2.7255,
             'A_s': self.cosmology['10^9 As'] * 10**-9,
             'n_s': self.cosmology['ns'],
@@ -50,37 +53,45 @@ class cosmology:
 
         self.rho_c_natural = self.background['(.)rho_cdm']
         self.rho_b_natural = self.background['(.)rho_b']
+        self.rho_crit_natural = self.background['(.)rho_crit']
+
 
         # rho_cb in units of h^2 Msol / Mpc^3
         self.rho_cb = (self.rho_c_natural + self.rho_b_natural) * conversion_factor
         self.f_rho_cb = interp1d(x = self.z_bg, y = self.rho_cb)
 
+        # rho_m,0
+        self.Omega_m = self.pkclass.get_current_derived_parameters(['Omega_m'])['Omega_m']
+        self.rho_m_0 = self.rho_crit_natural[-1] * self.Omega_m * conversion_factor
+
         # empty dictionary to store splines
         self.f_dln_sigma_cb = {}
         self.f_sigmas_cb = {}
 
+        self.f_dln_sigma_m = {}
+        self.f_sigmas_m = {}
 
 
 
     def Pm(self, k, z):
         """
-        k in units of 1/Mpc
+        k in units of h/Mpc
         z is reddshift
 
 
         returns linear matter power spectrum in units of Mpc^3 / h^3
         """
-        return self.pkclass.pk_lin(k, np.array([z])) * self.h ** 3
+        return np.array([self.pkclass.pk_lin(k_curr * self.h, np.array([z])) for k_curr in k]) * self.h ** 3
 
     def Pcb(self, k, z):
         """
-        k in units of 1/Mpc
+        k in units of h/Mpc
         z is reddshift
 
 
         returns linear matter power spectrum in units of Mpc^3 / h^3
         """
-        return self.pkclass.pk_cb_lin(k, np.array([z])) * self.h ** 3
+        return np.array([self.pkclass.pk_cb_lin(k_curr * self.h, np.array([z])) for k_curr in k]) * self.h ** 3
 
     def _sigma_cb(self, M, z):
         """
@@ -152,15 +163,86 @@ class cosmology:
         """
         return (self.f_dln_sigma_cb_dM(z))(M)
 
+    def _sigma_m(self, M, z):
+        """
+        M in units Msol / h
+            gets converted to Lagrangian scale of halo R with
+            R = (3 M / 4 pi rho_m,0) ** (1/3) # Mpc / h
+        z is redshift
 
-def B(a, M, σM, d, e, f, g):
+        returns sigma_m 
+        """
+        M_use = np.atleast_1d(M)
+        R = (3 * M_use / (4  * np.pi * self.rho_m_0)) ** (1/3) # Mpc / h
+        return np.array([self.pkclass.sigma(R = R_curr, z = z, h_units = True) for R_curr in R])
+
+    def f_sigma_m(self, z):
+        """
+        z is redshift
+
+        returns sigma interpolation using `scipy.interpolate.UnivariateSpline`
+        """
+        if(z in self.f_sigmas_m):
+            return self.f_sigmas_m[z]
+
+        Msample = np.logspace(12, 17, 1000)
+        sigma_m_samples = self._sigma_m(M = Msample, z = z)
+
+        f_log_sigma_m = UnivariateSpline(x = np.log(Msample), y = np.log(sigma_m_samples))
+
+        self.f_sigmas_m[z] = f_log_sigma_m
+        return self.f_sigmas_m[z]
+
+
+    def sigma_m(self, M, z):
+        """
+        M in units Msol / h
+            gets converted to Lagrangian scale of halo R with
+            R = (3 M / 4 pi rho_m,0) ** (1/3) # Mpc / h
+        z is redshift
+
+        returns sigma_m using spline
+        """
+        return np.exp((self.f_sigma_m(z))(np.log(M)))
+
+ 
+    def f_dln_sigma_m_dM(self, z):
+        """
+        z is redshift
+
+        returns dln(sigma_m)/dM spline using `scipy.interpolate.UnivariateSpline`
+        """
+        if(z in self.f_dln_sigma_m):
+            return self.f_dln_sigma_m[z]
+
+
+        f_log_sigma_m = self.f_sigma_m(z)
+        f_dlog_sigma_m_dlog_M = f_log_sigma_m.derivative()
+
+        #d log sig / d log M = M * d log sig / dM 
+        self.f_dln_sigma_m[z] = lambda M: 1/M * f_dlog_sigma_m_dlog_M(np.log(M))
+
+        return self.f_dln_sigma_m[z]
+
+    def dln_sigma_m_dM(self, M, z):
+        """
+        M in units Msol / h
+        z is redshift
+
+        returns dln(sigma_m)/dM for this mass and redshift in units h / Msol
+        """
+        return (self.f_dln_sigma_m_dM(z))(M)
+
+
+
+def B(d, e, f, g):
     oup = e**(d)*g**(-d/2)*gamma(d/2)
     oup += g**(-f/2)*gamma(f/2)
     return 2/oup
 
 
-def f_G(a, M, σM, d, e, f, g):
-    oup = B(a, M, σM, d, e, f, g)
+def f_G(σM, d, e, f, g):
+    oup = B(d, e, f, g)
     oup *= ((σM/e)**(-d)+σM**(-f))
     oup *= np.exp(-g/σM**2)
     return oup
